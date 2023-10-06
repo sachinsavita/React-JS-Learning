7ef3c630ac403b9d2e1e45b5a1503849c82ea879
Sachin Savita <36862754+sachinsavita@users.noreply.github.com>
2023-10-06 16:39:32 +0530

    Create FoodList.jsx

---
+++ FoodList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function FoodList() {
+  return (
+    <ul>
+      <li>Apples</li>
+      <li>Mango</li>
+      <li>Banana</li>
+    </ul>
+  );
+}
+export default FoodList;
