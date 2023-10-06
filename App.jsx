8f4db9080e34aa6058c145832c2be96a7470c166
Sachin Savita <36862754+sachinsavita@users.noreply.github.com>
2023-10-06 16:39:49 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Heading from "./Heading";
+import FoodList from "./FoodList";
+import Greeting from "./Greeting";
+
+function App() {
+  return (
+    <div>
+      <Greeting />
+      <Heading />
+      <FoodList />
+    </div>
+  );
+}
+
+export default App;
